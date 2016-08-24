7cb0601c02a5554ab7dc4c87d135e91b15ea26aa
Robien <romain@robien.com>
2016-08-24 09:57:49 +0200

    gen proto file script now browserify the result

--- proto/genProto.sh
+++ proto/genProto.sh
@@ -1,2 +1,6 @@
 protoc Connection.proto --csharp_out=../OW-unity/Assets/scripts/proto
 protoc --js_out=import_style=commonjs,binary:protoJS Connection.proto
+cd protoJS
+./npm.sh
+browserify Connection_pb.js | minify --js > connection.js
+cd -
