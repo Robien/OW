ed6d00a62322817edc77f6715a0caa7ce50b3088
Robien <robien@users.noreply.github.com>
2016-08-28 02:09:32 +0200

    backport Game proto file

--- proto/genProto.sh
+++ proto/genProto.sh
@@ -1,6 +1,7 @@
-protoc Connection.proto --csharp_out=../OW-unity/Assets/scripts/proto
-protoc --js_out=import_style=commonjs,binary:protoJS Connection.proto
+protoc Connection.proto Game.proto --csharp_out=../OW-unity/Assets/scripts/proto
+protoc --js_out=import_style=commonjs,binary:protoJS Connection.proto Game.proto
 cd protoJS
 ./npm.sh
 browserify Connection_pb.js | minify --js > connection.js
+browserify Game_pb.js | minify --js > game.js
 cd -
